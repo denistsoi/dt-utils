db860580a8fc2386322cca3c533ebef174b59825
Denis Tsoi <denistsoi@gmail.com>
2016-03-22 16:27:10 +0800

    ADD: throttle method;

--- index.js
+++ index.js
@@ -231,4 +231,26 @@ $.extend = function(){
   return obj;
 }
 
+// throttle on resize
+// usage
+// 
+// $.throttle("resize", "optimizedResize");
+// 
+// window.addEventListener("optimizedResize", function() {
+//    // custom event
+// });
+$.throttle = function(type. name, obj){
+  obj = obj || window;
+  var running = false;
+  var func = function() {
+    if (running) { return; }
+    running = true;
+      requestAnimationFrame(function() {
+        obj.dispatchEvent(new CustomEvent(name));
+        running = false;
+    });
+  };
+  obj.addEventListener(type, func);
+}
+
 exports = module.exports = $;
